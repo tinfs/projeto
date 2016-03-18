c33934c46c0b2aadfef6aba9a4c88208ac78af3d
DuarteF <duartefernandes11@gmail.com>
2016-03-18 17:17:49 +0000

    six

--- Popkorn/Popkorn/Form2.Designer.cs
+++ Popkorn/Popkorn/Form2.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(611, 496);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
